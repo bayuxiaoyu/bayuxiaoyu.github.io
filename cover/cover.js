91a61e3a4bfe1dc27138e89b52b06a75556d6091
bayuxiaoyu <2782303429@qq.com>
2022-04-18 21:30:17 +0800

    first try

---
+++ cover/cover.js
@@ -0,0 +1,19 @@
+
+window.onload = function(){
+
+    const getin = document.getElementById("getin");
+
+    getin.onclick = function(){
+        window.location.href = '../home/home.html';
+        
+    }
+
+
+
+
+
+
+
+
+
+}
